15b4e6c4bc20d9a5e1658c6a5beadd53519f5d07
JayD <jaydeepjd.1125@gmail.com>
2020-10-11 00:45:08 +0530

    Sqrs

---
+++ sqrs.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sqr(int n)
+{
+	cout<<"Squares\n";
+	for (int i=1; i<=n ; i++)
+	{
+		cout<<i*i<< "\n";
+	}
+}
+
+int main()
+{
+	int n = 20;
+	sqr(n);
+	return 0;
+}
